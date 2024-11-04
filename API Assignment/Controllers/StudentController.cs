02eabf94b1c1274c0027c00fd507c7dfa33c4e4e
Mary Mungai <mungaimw@mail.uc.edu>
2024-11-04 01:49:11 -0500

    Student and Controller Created

---
+++ API Assignment/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using API_Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Assignment.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Student student = new Student(5, "Mary");
+            
+            return Ok(student);
+        }
+    }
+}
---
+++ API Assignment/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace API_Assignment.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }   
+
+        public Student() { }
+        public Student(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+    }
+}
